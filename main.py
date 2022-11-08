69484614d1842670be488828c968d4a96f41dd75
xxshubhamxx <shubham2002garg@gmail.com>
2022-11-08 21:10:48 +0530

    added code to select currency and dates and display the result with web frontend, not yet selecting based on time

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from flask import redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectMultipleField, widgets
-from wtforms import SelectField
 from wtforms.validators import DataRequired
-import datetime
-from wtforms.fields.html5 import DateField
 import os
 import pandas as pd
-import plotly.express as px
+
 import io
 import random
 from flask import Response
@@ -37,10 +34,8 @@ def get_choices():
     return df
 
 class NameForm(FlaskForm):
-    cur1 = SelectField(u'Select Currency 1:', choices=get_choices().columns[1:], render_kw={'style': 'width: 30ch; height: 5ch;'})
-    # cur2 = SelectMultipleField(u'Select Currency 2:', choices=get_choices().columns[1:], render_kw={'style': 'width: 10ch; height: 30ch;'} )
-    dt1 = DateField('DatePicker', format='%Y-%m-%d', render_kw={'style': 'width: 30ch; height: 5ch;'})
-    dt2 = DateField('DatePicker', format='%Y-%m-%d', render_kw={'style': 'width: 30ch; height: 5ch;'})
+    cur1 = SelectMultipleField(u'Select Currency 1:', choices=get_choices().columns[1:], render_kw={'style': 'width: 10ch; height: 30ch;'})
+    cur2 = SelectMultipleField(u'Select Currency 2:', choices=get_choices().columns[1:], render_kw={'style': 'width: 10ch; height: 30ch;'} )
     submit = SubmitField('Submit')
     
 @app.route('/', methods=['GET', 'POST'])
@@ -52,24 +47,30 @@ def index1():
     if form.validate_on_submit():
         
         
-        cur1 = form.cur1.data
-        dt1 = form.dt1.data #
-        dt2 = form.dt2.data #
-        
-        print("dt1:",dt1)
-        print("dt2:",dt2)
-        
-        # cur2 = form.cur2.data[0]
-        # df = df[[cur1,cur2,'Date']]
-        df = df[[cur1,'Date']]
+        cur1 = form.cur1.data[0]
+        cur2 = form.cur2.data[0]
+        df = df[[cur1,cur2,'Date']]
         df.dropna(inplace=True)
         df.set_index('Date', inplace=True)
         print(df)
         
+        def create_figure():
+            fig = Figure()
+            axis = fig.add_subplot(1, 1, 1)
+            xs = df.iloc[:,0].values
+            ys = df.iloc[:,1].values
+            axis.plot(xs, ys)
+            return fig
+        
+        
+        fig = create_figure()
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        # return Response(output.getvalue(), mimetype='image/png')
     
     
         
-        return render_template('output.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+        return render_template('output.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
     
     return render_template('index.html', title='My Form', form=form, data=data)
 
